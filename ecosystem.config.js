9def9f4badd03742f29157b59d4a787ef8dcfdb7
Dzmitry Panamarenka <dzmitry_panamarenka@epam.com>
2019-02-27 18:56:25 +0300

    update eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       production: {
         user: 'ubuntu',
         host: 'ec2-18-223-109-21.us-east-2.compute.amazonaws.com',
-        key: '~/.ssh/wolvesg.pem',
+        key: '~/.ssh/wolvesgg.pem',
         ref: 'origin/master',
         repo: 'git@github.com:dzmitrypanamarenka/wolves_g.git',
         path: '/home/ubuntu/wolves_g',
